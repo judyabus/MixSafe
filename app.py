f3432b372a000a445ae589efeae0cb11719ce5bf
Judy Abusteit <jabustei@ualberta.ca>
2024-06-04 22:36:51 -0600

    <commit message>

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def results():
     return render_template('data.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False)
+
